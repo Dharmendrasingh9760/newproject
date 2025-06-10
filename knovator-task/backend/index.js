5a19ee635d7bf57b3319c7275a0f0e386ef087f7
Dharmendrasingh9760 <152598648+Dharmendrasingh9760@users.noreply.github.com>
2025-06-10 15:11:50 +0530

    Update index.js

--- knovator-task/backend/index.js
+++ knovator-task/backend/index.js
@@ -2,5 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+// Handle root
 app.get("/", (req, res) => res.send("Hello from Node.js Backend"));
+
+// Handle /api
+app.get("/api", (req, res) => res.send("Hello from /api route via NGINX"));
+
 app.listen(port, () => console.log(`App running on port ${port}`));
